535d17a6b8697a90d6217eefc400853dc8f3da08
cnormie <github.throwaway.1234321@gmail.com>
2020-01-25 19:49:43 +0000

    tacoworld: plenty o tacos

--- tacoworld.py
+++ tacoworld.py
@@ -1,2 +1,2 @@
 print("I, Andrew Stewart, want tacos more than *anyone*, especially more than carl!)
-print("I, carl norman, want tacos!!")
+print("I, carl norman, want tacos, so it's good there are enough to share!")
